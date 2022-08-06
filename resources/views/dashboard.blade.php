e7c8d9771e0ea09c091f3557e4a08fb5f796f52f
Matthew Brain <admin@CEO-HOME-PC.mbdev.org>
2022-08-06 21:10:55 +0100

    Development of dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,163 @@
+<?php 
+$username = Auth::user()->PLAYER_NAME;
+$userdetails = DB::table('staffcoreui_accounts')->where('PLAYER_NAME', '=', 'mbdevmatthew')->get();
+ $bancount = DB::table('ReportSystem_banhistory')->where('BANNED_UUID', '=', Auth::user()->UUID)->get();
+ $bancount = count($bancount);
+
+ // $mutecount = DB::table('ReportSystem_mutesdb')->where('MUTED_UUID', '=', Auth::user()->UUID)->get();
+ // $mutecount = count($bancount);
+ $mutecount = 0;
+
+
+ ?>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="container-fluid">
+            <div class="card card-widget widget-user">
+              
+                    <div class="widget-user-header bg-danger" >
+                      <h3 class="widget-user-username">
+                        
+{{Auth::user()->PLAYER_NAME}} <br><span style="color: #ffffff !important;" class="text-info text-bold" title="">LAST SERVER LOGIN: {{Auth::user()->LAST_LOGIN}}</span>                      </h3>
+                      <h5 class="widget-user-desc">
+
+</h5>
+                    </div>
+                    <div class="widget-user-image">
+
+                      <img class="" style="border: none !important;" alt="User Avatar" src="https://minotar.net/bust/{{Auth::user()->PLAYER_NAME}}/500.png">
+
+                    </div>
+                    <div class="card-footer">
+                      <div class="row">
+                        <div class="col-sm-3 border-right">
+                          <div class="description-block">
+                            <h5 class="description-header">Bans</h5>
+                            <h5 class="description-header">{{$bancount}}</h5>
+                            <span class="description-text"></span>
+                          </div>
+                          <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-3 border-right">
+                          <div class="description-block">
+                            <h5 class="description-header">4</h5>
+                            <span class="description-text">MUTES</span>
+                          </div>
+                          <!-- /.description-block -->
+                        </div>
+                        <div class="col-sm-3 border-right">
+                          <div class="description-block">
+                            <h5 class="description-header">3</h5>
+                            <span class="description-text">REPORTS</span>
+                          </div>
+                          <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-3">
+                          <div class="description-block">
+                            <h5 class="description-header">0</h5>
+                            <span class="description-text">WARNS</span>
+                          </div>
+                          <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
+                      </div>
+                      <!-- /.row -->
+                    </div>
+                                </div>
+
+            
+
+
+
+
+
+                <div class="content-header">
+                  <div class="row mb-2">
+                    <div class="col-sm-6">
+                      <h1 class="m-0 text-dark">Your Tasks</h1>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="d-flex flex-wrap w-100">
+                  <div class="card card-outline card-danger w-100">
+                    <div class="table-responsive">
+                      <table class="table table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">#-ID</th>
+                            <th scope="col">Assigned by</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                                                        <tr>
+                                <th scope="row" colspan="4">
+                                  <div class="alert alert-success" role="alert">
+                                    All set! You dont have any tasks for now!
+                                  </div>
+                                </th>
+                              </tr>
+                                                      </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+                
+            <div class="content-header">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0 text-dark">Your Reports</h1>
+                </div>
+              </div>
+            </div>
+
+            <div class="d-flex flex-wrap w-100">
+              <div class="card card-outline card-danger w-100">
+                <div class="table-responsive">
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">#-ID</th>
+                        <th scope="col">Reported</th>
+                        <th scope="col">Reason</th>
+                        <th scope="col">Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                                                  <tr>
+                              <th scope="row">3</th>
+                              <td>
+CrazySeppe</td>
+                              <td>hacking</td>
+                              <td>
+                                                                    <span class="font-weight-bold">
+                                      <i class="fas fa-caret-up text-success"></i>
+                                      <span class="text-muted">Claimed</span>
+                                    </span>
+                                                                  </td>
+                            </tr>
+                                                </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+          </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<?php  ?>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <!-- Styles -->
+          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="http://mcportal.mbdev.org/adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="http://mcportal.mbdev.org/adminlte/dist/css/adminlte.min.css">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="http://mcportal.mbdev.org/adminlte/plugins/toastr/toastr.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+</html>
